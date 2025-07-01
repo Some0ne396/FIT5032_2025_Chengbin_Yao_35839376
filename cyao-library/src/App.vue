e16c0b60144a6e7809319d09f0c12222c3339da0
Someguy324 <81729916+Someguy324@users.noreply.github.com>
2025-07-01 16:47:51 +0800

    Assessed Lab 2
    Finished all task for Assessed Lab 2

---
+++ cyao-library/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import JSON from './components/JSON.vue';
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div>
+    <JSON></JSON>
+  </div>
+</template>
+
+<style scoped>
+</style>
